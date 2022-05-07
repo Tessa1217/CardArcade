1881e510aeb674f73ff2e150e1b49a6752ebe7ca
Tessa <kwonyuj1996@gmail.com>
2022-05-07 21:33:45 +0900

    second commit - defining player's role

--- card/src/main/java/co/arcade/card/player/Player.java
+++ card/src/main/java/co/arcade/card/player/Player.java
@@ -4,6 +4,13 @@ import co.arcade.card.carddeck.Card;
 
 public interface Player {
 
+//	플레이어의 동작 
+//	베팅하기 - 블랙잭, 원카드
+//	카드 받기 - 블랙잭, 원카드
+//	카드 한 장 뽑기 - 블랙잭, 원카드
+//	카드 내기 - 원카드
+//	힛 앤 스탠드 - 블랙잭
+//	베팅 금액 받기 - 블랙잭, 원카드 
 	Card hit();
 
 }
